097b020293109b426520c428f9c227c6cab7f334
Mkrtich <amv.mkrtich@gmail.com>
2024-05-11 00:21:48 +0400

    initial commit

---
+++ dist/assets/js/main.js
@@ -0,0 +1,52 @@
+
+function toggleClassOnScroll(className){
+    const elements = document.querySelectorAll(`.${className}`);
+    if(!elements.length) return;
+
+    elements.forEach(el => {
+        let counter = 0;
+        for(const child of el.children){
+            child.style.setProperty('--anim-delay', `${counter * .1}s`);
+            counter++;
+        }
+
+        const {scrollTop, clientHeight} = document.documentElement;
+
+        const topElementToTopViewport = el.getBoundingClientRect().top;
+
+        if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 1){
+            el.classList.add('is-visible');
+        }
+    });
+}
+
+toggleClassOnScroll('animate-elem');
+
+
+function closeMenu(){
+    document.body.classList.remove('mobile-menu-open');
+}
+
+document.body.addEventListener('click', e => {
+    if(!e.target.closest('.mobile-nav') && !e.target.closest('.menu-toggle')){
+        closeMenu()
+    }
+})
+
+// mobile menu toggle
+const menuTogglers = document.querySelectorAll('.menu-toggle');
+if(menuTogglers.length) {
+    menuTogglers.forEach(menuToggler => {
+        toggleMobileMenu(menuToggler)
+    })
+}
+
+
+function toggleMobileMenu(menuToggler, button){
+    menuToggler.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.body.classList.toggle('mobile-menu-open');
+    });
+}
+
+// alert(`Your screen resolution is: ${window.innerWidth} X ${window.innerHeight}`)
---
+++ readme.txt
@@ -0,0 +1,2 @@
+home testimonial slider autoplay pause on hover slide 2 +
+לשלב הבא  - as a next button for tabs
